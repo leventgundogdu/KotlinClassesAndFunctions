8574ab8c43eed21bdf24f64781f9521b46a3892c
Levent Gündoğdu <gundogdulevent@outlook.com>
2025-02-07 18:53:10 +0300

    Classes Commit 1
    Simpson class created. TextViews have been implemented into MainActivity.

--- app/src/main/java/com/leventgundogdu/classesandfunctions/MainActivity.kt
+++ app/src/main/java/com/leventgundogdu/classesandfunctions/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.leventgundogdu.classesandfunctions
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //OnCreate'den once bir tanim yapilmaz, yapilirsa uygulama çöker. Uygulama, lateinit var'ı tanımlamazsan da çöker.
+    lateinit var myTextView : TextView
+    lateinit var myButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +25,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        myTextView = findViewById(R.id.textView)
+        myButton = findViewById(R.id.button)
+
+//        myButton.setOnClickListener {
+//            myTextView.text = "button clicked 2"
+//        }
+
         //Functions: 1) code block, reusable 2) input 3) output - return
 
-//        println("Hello Kotlin")
         test()
         mySum(10, 15) //Fonksiyon tipi Unit ise bir deger dondurmez
 
@@ -30,6 +42,15 @@ class MainActivity : AppCompatActivity() {
 
         println(sumResult)
         println(result)
+
+        //Class
+
+        val homer = Simpson()
+        homer.name = "Homer Simpson"
+        homer.age = 50
+        homer.job = "Nuclear"
+
+        println(homer.name)
 
     }
 
@@ -45,11 +66,16 @@ class MainActivity : AppCompatActivity() {
 //        println(x * y)
         val result = x * y
 
-        val myTextView = findViewById<TextView>(R.id.textView)
-        myTextView.text = "Result: $result"
+        myTextView.text = "Result: ${result}"
 
         return result //return'den sonra kod yazilmaz cunku kod, return'den sonra okunmaz
     }
+
+    fun buttonClicked(view : View) {
+        myTextView.text = "Button Clicked"
+    }
+
+
 
 
 
---
+++ app/src/main/java/com/leventgundogdu/classesandfunctions/Simpson.kt
@@ -0,0 +1,10 @@
+package com.leventgundogdu.classesandfunctions
+
+class Simpson {
+
+    //Properties
+    var name = ""
+    var age = 0
+    var job = ""
+
+}
